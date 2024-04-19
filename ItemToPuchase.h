b359061df8832af423a2ed88ba4352135e50d4b0
msappel4 <167126884+msappel4@users.noreply.github.com>
2024-04-19 11:25:03 -0500

    Create ItemToPuchase.h
    Finalized ItemToPuchase.h

---
+++ ItemToPuchase.h
@@ -0,0 +1,24 @@
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
+
+#include <string>
+using namespace std;
+
+class ItemToPurchase{
+   
+   public:
+      ItemToPurchase (string itemName, int itemPrice, int itemQuantity);
+      void SetName(string itemName);
+      string GetName();
+      void SetPrice(int itemPrice);
+      int GetPrice();
+      void SetQuantity(int itemQuantity);
+      int GetQuantity();
+      
+   private:
+      string itemName;
+      int itemPrice;
+      int itemQuantity;
+      
+};
+#endif
